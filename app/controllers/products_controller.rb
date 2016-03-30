f2fdc38cf282b78a4957d1e717430e8d1f862624
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-03-30 12:14:06 -0400

    Add create method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,12 @@ class ProductsController < ApplicationController
     Product.destroy(params[:id])
     render json: {success: true}, status: :ok
   end
+  def create
+    @product = Product.create(product_params)
+    render json: @product, status: :ok
+  end
+  private
+  def product_params
+    params.require(:product).permit(:name, :image_url, :cost, :quantity, :country)
+  end
 end
